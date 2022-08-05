7fcc6a6528a693f39654ccf49dbc638aeb66e1f6
Vovuk <109819093+Vovuk@users.noreply.github.com>
2022-08-05 15:29:30 +0300

    Driver elif

--- driver.py
+++ driver.py
@@ -1,14 +1,11 @@
 speed = int(input("Input speed drive: "))
 if speed > 200:
     print("Speed isn't correct!")
+elif (speed > 60):
+    print("Driver violets!")
+elif (speed == 0):
+    print("The car is standing!")
+elif (speed < 0):
+    print("Speed isn't correct!")
 else:
-    if speed > 60:
-        print("Driver violets!")
-    else:
-        if speed == 0:
-            print("The car is standing!")
-        else:
-            if speed < 0:
-                print("Speed isn't correct!")
-            else:
-                print("Driver not violets")
+    print("Driver not violets")
