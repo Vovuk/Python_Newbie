839b9926351bae571307ebbd20876bc6b5e92b0f
Vovuk <109819093+Vovuk@users.noreply.github.com>
2022-08-10 20:30:49 +0300

    2digit3number

---
+++ 2digit3numbers.py
@@ -0,0 +1,11 @@
+from locale import format_string
+
+
+number = int(input("Input third digit number: "))
+
+# a = number % 100
+# b = a // 10
+
+strnumber = str(number)
+print("Second digit:", strnumber[1])
+
